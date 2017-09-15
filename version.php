9e261f71a2afaaf5dc0af30f0d32bdb26eb14dca
Trevor <trevor.mccready@cornerstone.edu>
2017-09-15 14:48:16 -0400

    Updated date in version.php

--- version.php
+++ version.php
@@ -44,7 +44,7 @@
  *
  **/
 
-$plugin->version = 2017080901;
+$plugin->version = 2017091500;
 $plugin->requires = 2015111610;	// Moodle 3.0.10 is required
 $plugin->component = 'block_intelligent_learning';
 $plugin->release   = '3.0.0 (Build: 20170809)';
